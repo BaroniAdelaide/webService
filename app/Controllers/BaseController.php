d88002343fde233cfdeea0db2ffb9e930d960e22
Adelaide Baroni <sadelaide.baroni@itis.pr.it>
2024-04-24 19:28:37 +0200

    modifiche headers in BaseController

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -55,5 +55,9 @@ abstract class BaseController extends Controller
 
         // E.g.: $this->session = \Config\Services::session();
         header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+        header("Access-Control-Max-Age: 3600");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     }
 }
